683924181647e3c868639738288aa0b419fb402b
CrazyStudiosYT <ijeomahorjia@gmail.com>
2022-07-07 21:05:42 +0100

    Main things

--- src/org/nymo/Main.java
+++ src/org/nymo/Main.java
@@ -1,7 +1,10 @@
 package org.nymo;
 
+import org.nymo.engine.Window;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Window window = Window.get();
+        window.run();
     }
 }
